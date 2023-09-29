bf7e9c091d81fb8d3705961fa3f5ef6e82e285e8
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-09-29 16:50:51 +0200

    tweak viewer

--- src/anywidget_3dviewer/__init__.py
+++ src/anywidget_3dviewer/__init__.py
@@ -4,14 +4,11 @@ import anywidget
 from traitlets import Unicode
 
 try:
-    __version__ = importlib.metadata.version("ipymolecularnodes")
+    __version__ = importlib.metadata.version("anywidget_3dviewer")
 except importlib.metadata.PackageNotFoundError:
     __version__ = "unknown"
 
-
-class BlenderInteractiveWidget(anywidget.AnyWidget):
+class Viewer(anywidget.AnyWidget):
 
     gltf_data = Unicode("").tag(sync=True)
-
-    _esm = pathlib.Path(__file__).parent / "static" / "interactive_widget.js"
-    # _css = pathlib.Path(__file__).parent / "static" / "widget.css"
+    _esm = pathlib.Path(__file__).parent / "static" / "viewer.js"
